d2cc3fef2034a4b618b9d7947da2693ab264c526
Kentaro Kuribayashi <kentarok@gmail.com>
2016-12-26 00:21:25 +0900

    Fix errors

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
 return [
-    "site_title"   => "Soushi: Bimodal Site Generator Powered by PHP
-
-",
+    "site_title"   => "Soushi: Bimodal Site Generator Powered by PHP",
     "template_dir" => dirname(__FILE__) . "/templates",
     "source_dir"   => dirname(__FILE__) . "/source",
 ];
--- src/Command/Init.php
+++ src/Command/Init.php
@@ -50,7 +50,7 @@ EOS;
         $content = <<<'EOS'
 <?php
 return [
-    "site_title"   => "My Homepage",php
+    "site_title"   => "My Homepage",
     "template_dir" => dirname(__FILE__) . "/templates",
     "source_dir"   => dirname(__FILE__) . "/source",
 ];
